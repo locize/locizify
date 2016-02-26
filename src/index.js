a8f75a1111ca3e2d25a514ce8244af3a4ea48499
Jan Muehlemann <jan.muehlemann@gmail.com>
2016-02-26 13:06:45 +0100

    updated build output

--- src/index.js
+++ src/index.js
@@ -1,132 +1,98 @@
-import * as utils from './utils';
+import i18next from 'locize';
+import LngDet from 'i18next-browser-languagedetector/lib';
 
-// https://gist.github.com/Xeoncross/7663273
-function ajax(url, options, callback, data, cache) {
-  // Must encode data
-  if(data && typeof data === 'object') {
-    var y = '', e = encodeURIComponent;
-    for (var m in data) {
-      y += '&' + e(m) + '=' + e(data[m]);
-    }
-    data = y.slice(1) + (!cache ? '&_t=' + new Date : '');
-  }
+import Observer from 'i18nextify/lib/Observer';
 
-  try {
-    var x = new (XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
-    x.open(data ? 'POST' : 'GET', url, 1);
-    if (!options.crossDomain) {
-      x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    }
-    x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    x.onreadystatechange = function() {
-      x.readyState > 3 && callback && callback(x.responseText, x);
-    };
-    x.send(data);
-  } catch (e) {
-    window.console && console.log(e);
-  }
-};
-
-// ajax.uriEncode = function(o) {
-//     var x, y = '', e = encodeURIComponent;
-//     for (x in o) y += '&' + e(x) + '=' + e(o[x]);
-//     return y.slice(1);
-// };
-//
-// ajax.collect = (a, f) {
-//     var n = [];
-//     for (var i = 0; i < a.length; i++) {
-//         var v = f(a[i]);
-//         if (v != null) n.push(v);
-//     }
-//     return n;
-// };
-//
-// ajax.serialize = function (f) {
-//     function g(n) {
-//         return f.getElementsByTagName(n);
-//     };
-//     var nv = function (e) {
-//         if (e.name) return encodeURIComponent(e.name) + '=' + encodeURIComponent(e.value);
-//     };
-//     var i = collect(g('input'), function (i) {
-//         if ((i.type != 'radio' && i.type != 'checkbox') || i.checked) return nv(i);
-//     });
-//     var s = collect(g('select'), nv);
-//     var t = collect(g('textarea'), nv);
-//     return i.concat(s).concat(t).join('&');
-// };
-//
+import docReady from 'i18nextify/lib/docReady';
+import renderer from 'i18nextify/lib/renderer';
+import { missingHandler } from 'i18nextify/lib/missingHandler';
 
 function getDefaults() {
   return {
-    loadPath: '/locales/{{lng}}/{{ns}}.json',
-    addPath: 'locales/add/{{lng}}/{{ns}}',
-    allowMultiLoading: false,
-    parse: JSON.parse,
-    crossDomain: false
-  };
+    autorun: true,
+    ele: document.body,
+    ignoreTags: ['SCRIPT'],
+    nsSeparator: '#||#',
+    keySeparator: '#|#',
+    debug: window.location.search && window.location.search.indexOf('debug=true') > -1,
+    saveMissing: window.location.search && window.location.search.indexOf('saveMissing=true') > -1
+  }
 }
 
-class Backend {
-  constructor(services, options = {}) {
-    this.init(services, options);
 
-    this.type = 'backend';
+// auto initialize on dom ready
+let domReady = false;
+let initialized = false;
+docReady(function() {
+  domReady = true;
+  if (!initialized) init();
+});
+
+// extend i18next with default extensions
+i18next.use(LngDet);
+
+// log out missings
+i18next.on('missingKey', missingHandler);
+
+// store last init options - for case init is called before dom ready
+let lastOptions = {};
+
+
+function init(options = {}) {
+  options = {...getDefaults(), ...lastOptions, ...options};
+
+  // delay init from domReady
+  if (!options.ele) {
+    delete options.ele;
+    lastOptions = options;
+    return;
   }
 
-  init(services, options = {}) {
-    this.services = services;
-    this.options = utils.defaults(options, this.options || {}, getDefaults());
-  }
+  initialized = true;
+  let renderers = [];
 
-  readMulti(languages, namespaces, callback) {
-    let url = this.services.interpolator.interpolate(this.options.loadPath, { lng: languages.join('+'), ns: namespaces.join('+') });
+  function addRenderers() {
+    let ele = options.ele;
+    let children = ele.children;
 
-    this.loadUrl(url, callback);
-  }
-
-  read(language, namespace, callback) {
-    let url = this.services.interpolator.interpolate(this.options.loadPath, { lng: language, ns: namespace });
-
-    this.loadUrl(url, callback);
-  }
-
-  loadUrl(url, callback) {
-    ajax(url, this.options, (data, xhr) => {
-      const statusCode = xhr.status.toString();
-      if (statusCode.indexOf('5') === 0) return callback('failed loading ' + url, true /* retry */);
-      if (statusCode.indexOf('4') === 0) return callback('failed loading ' + url, false /* no retry */);
-
-      let ret, err;
-      try {
-        ret = this.options.parse(data);
-      } catch (e) {
-        err = 'failed parsing ' + url + ' to json';
+    for (var i = 0; i < children.length; i++) {
+      let c = children[i];
+      if (options.ignoreTags.indexOf(c.tagName) < 0 && !c.attributes.translated) {
+        let r = renderer(c, observer);
+        renderers.push(r);
+        r.render();
       }
-      if (err) return callback(err, false);
-      callback(null, ret);
-    });
+    }
   }
 
-  create(languages, namespace, key, fallbackValue) {
-    if (typeof languages === 'string') languages = [languages];
+  let observer = new Observer(options.ele);
 
-    let payload = {};
-    payload[key] = fallbackValue || '';
+  observer.on('changed', (mutations) => {
+    renderers.forEach(r => r.debouncedRender());
+    addRenderers();
+  });
 
-    languages.forEach(lng => {
-      let url = this.services.interpolator.interpolate(this.options.addPath, { lng: lng, ns: namespace });
+  let todo = 1;
+  if (!domReady) todo++;
+  if (options.autorun === false) todo++;
 
-      ajax(url, this.options, function(data, xhr) {
-        //const statusCode = xhr.status.toString();
-        // TODO: if statusCode === 4xx do log
-      }, payload);
-    });
+  function done() {
+    todo = todo - 1;
+    if (!todo) {
+      addRenderers();
+      if (options.ele.style && options.ele.style.display === 'none') options.ele.style.display = 'block';
+    }
   }
+
+  i18next.init(options, done);
+
+  if (options.autorun !== false && !domReady) {
+    docReady(done);
+  }
+  if (options.autorun === false) return { start: done };
 }
 
-Backend.type = 'backend';
-
-
-export default Backend;
+export default {
+  init: init,
+  i18next: i18next
+}
